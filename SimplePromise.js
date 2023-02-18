96038189118d33c270a8670bef8568394d39b053
Andrii Dobronos <andriidobronos@gmail.com>
2023-02-18 14:21:01 +0200

    corrected homework

--- SimplePromise.js
+++ SimplePromise.js
@@ -2,50 +2,12 @@
 //Написати функцію, яка повертає проміс, який виконається через
 // 2 секунди після виклику з результатом "Готово"
 
-//Якщо час затримки та дію alert плануємо передавати як параметри :
-function returnPromise1(delayInSeconds){
-    return new Promise(function(resolve){
-        setTimeout(resolve, delayInSeconds*1000);
-    });
+function returnPromiseAfter2Seconds() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() { resolve("Готово")
+            }, 2000)
+    })
 }
-returnPromise1(2).then(function(){alert("Готово from promise1");});
-
-//Якщо тільки дію alert з конкретним значенням плануємо
-// передавати як параметр :
-function returnPromise2(){
-    return new Promise(function(resolve){
-        setTimeout(() => {
-            resolve()
-        }, 2000)
-    });
-}
-returnPromise2().then(() => alert('Готово from promise2'))
-
-//Якщо не плануємо змінювати час затримки та зміст повідомлення :
-function returnPromise3(){
-    return new Promise(function(resolve){
-        setTimeout(() => {
-            resolve('Готово from promise3')
-        }, 2000)
-    });
-}
-returnPromise3().then((value) => {alert(value)})
-
-//Якщо може виникнути помилка та ми плануємо змінювати дію функціі
-// коли її викликаємо(наприклад : alert замінити на console.log)
-function returnPromise4(){
-    return new Promise(function(resolve,reject){
-        setTimeout(() => {
-            if(resolve) {
-                resolve('Готово from promise4')
-            }else{
-                reject("Помилка! from promise4")
-            }
-        }, 2000)
-    });
-}
-returnPromise4().then(function(value) {
-    alert(value); // Успіх!
-}, function(reason) {
-    alert(reason); // Помилка!
-})
+//returnPromiseAfter2Seconds().then(function(value) {
+//    console.log(value)
+//})
